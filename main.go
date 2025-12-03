ffd1fc64c5bdcb9a0ceb7c03b95ee099ec526882
Pablo Pomodoro <p.pomodoro@posteo.com>
2025-12-04 00:14:49 +0100

    feat: day 2, part 2

--- main.go
+++ main.go
@@ -19,20 +19,59 @@ func main() {
 		end, _ := strconv.Atoi(strings.Split(scope, "-")[1])
 
 		for id := start; id <= end; id++ {
-			magnitude := len(strconv.Itoa(id))
-			if magnitude%2 != 0 {
+
+			idString := strconv.Itoa(id)
+			idStringLength := len(idString)
+
+			if idStringLength < 2 {
 				continue
 			}
 
-			midway := magnitude / 2
-			firstHalf := strconv.Itoa(id)[:midway]
-			secondHalf := strconv.Itoa(id)[midway:]
+			singleDigit := true
+			for i := 1; i < idStringLength; i++ {
 
-			if firstHalf == secondHalf {
+				if idString[0] != idString[i] {
+					singleDigit = false
+					break
+				}
+			}
+			if singleDigit {
+				idsSum += id
+				continue
+			}
+
+			if idStringLength < 4 {
+				continue
+			}
+
+			repeating := false
+			for j := 2; j < idStringLength; j++ {
+				segment := idString[:j]
+
+				segmentLength := len(segment)
+
+				if idStringLength%segmentLength != 0 {
+					continue
+				}
+
+				var nextSegment string
+				repeating = true
+				for k := segmentLength; k < idStringLength; k += segmentLength {
+					nextSegment = idString[k : k+segmentLength]
+
+					if segment != nextSegment {
+						repeating = false
+						break
+					}
+				}
+				if repeating {
+					break
+				}
+			}
+			if repeating {
 				idsSum += id
 			}
 		}
 	}
-
 	fmt.Println(idsSum)
 }
